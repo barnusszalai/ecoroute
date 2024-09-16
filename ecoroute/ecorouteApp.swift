fd2860b0732345875164215faaa41ac070248c32
Barnabas Szalai <barney.szalai@gmail.com>
2024-09-16 17:11:37 +0200

    Initial Commit

---
+++ ecoroute/ecorouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecorouteApp.swift
+//  ecoroute
+//
+//  Created by Barni Szalai on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ecorouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
